dc29b9533b4dfbb446d7f829b0112d6e307ea847
neekesh <nikesthapa20@gmail.com>
2023-12-01 22:37:10 +0545

    feat: added join stream page

---
+++ src/pages/JoinStreampage/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {JoinStream } from "../../components";
+
+const JoinStreamPage = () => {
+  return <JoinStream/>
+}
+
+export default JoinStreamPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Homepage} from "./Homepage"
 export {default as Streampage} from "./Streampage"
+export {default as JoinStreamPage} from "./JoinStreampage"
