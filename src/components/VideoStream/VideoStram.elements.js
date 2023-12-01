98b8aa8df04aa3078595928e2222ccf338dfc3aa
neekesh <nikesthapa20@gmail.com>
2023-12-01 22:37:54 +0545

    fix: refactor

--- src/components/VideoStream/VideoStram.elements.js
+++ src/components/VideoStream/VideoStram.elements.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 import { Container } from "../../globalStyles"
-import { VscDebugStart, VscDebugPause } from "react-icons/vsc";
+
 
 export const StreamContainer = styled(Container)`
     display: flex;
@@ -13,26 +13,4 @@ export const StreamContainer = styled(Container)`
 export const VideoContainer = styled.video`
     width: auto;
     height: 500px;
-`
-
-export const LiveButton = styled.button`
-    background-color: grey;
-    font-size: 1rem;
-    width: auto;
-    padding: 0.5rem;
-    color: #F0F8FF;
-    border-radius: 0.5rem;
-    border: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 0.5rem;
-`
-
-export const PlayButton = styled(VscDebugStart)`
-background-color: grey;
-`
-
-export const PauseButton = styled(VscDebugPause)`
-background-color: grey;
 `
